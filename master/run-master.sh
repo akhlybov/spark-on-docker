bdb6afa90dfc40dcc08105c5ceb29b64d27f8712
Alex Khlybov <a.khlybov@smartweb.com.ua>
2020-02-27 16:51:06 +0200

    Finish spark-master implementation

--- master/run-master.sh
+++ master/run-master.sh
@@ -1,7 +1,19 @@
-zookeeper_urls = $1
-if [zookeeper_urls == "None"]
+if [[ $RECOVERY_MODE = "None" ]]
 then
-    hostip = $(ip route show | awk '/default/ {print $3}')
+    /bin/bash /spark/sbin/start-master.sh -p $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+else
+    if [[ $RECOVERY_MODE = "ZOOKEEPER" ]]
+    then
+        if [[ $ZOOKEEPER_URLS = "None" ]]
+        then
+            # resolves host machine ip from within container assuming Zookeeper is not running on Docker
+            ZOOKEEPER_URL="$(ip route show | awk '/default/ {print $3}'):2181"
+        fi
+        export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=$RECOVERY_MODE -Dspark.deploy.zookeeper.url=$ZOOKEEPER_URL"
+        /bin/bash /spark/sbin/start-master.sh -p $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+    else
+        echo "Invalid recovery mode: $RECOVERY_MODE"
+    fi
 fi
 
-export SPARK_DAEMON_JAVA_OPTS='-Dspark.deploy.recoveryMode="${RECOVERY_MODE}" -Dspark.deploy.zookeeper.url="$ZOOKEEPER_URL"'
+tail -f /dev/null
